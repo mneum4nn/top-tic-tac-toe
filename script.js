6e07b187aefa000a0841d3f160d83d27178f552e
mneum4nn <n3um4nn@proton.me>
2023-11-29 12:39:23 -0600

    stylize gameboard and remove row divs

--- script.js
+++ script.js
@@ -3,39 +3,36 @@
     const gameDisplay = document.querySelector('.game-page'); 
     const playBtn = document.getElementById('play-btn');
     const welcomeDisplay = document.querySelector('.welcome-page');
-
+    const view = document.getElementById('view');
     //EVENT LISTENERS 
         playBtn.addEventListener('click', () => {
-            makeGameboard();
+            game.createBoard();
     });    
 
 
 //FACTORY FUNCTION AREA
 //TODO: create gameboard object with board as a local array
     //include the gameboards own methods within this function, creating them as IIFE when possible
- const game = ( function () {
+const game = ( function () {
+    let gameboard = [];
     const createBoard = function (){
-        let gameboard = [];
-        for (let i = 0 ; i < 3; i++){
-            //create each row
-            const boardRow = document.createElement('div');
-                boardRow.classList.add('grid-row');
-                boardRow.setAttribute('id', 'row'+i);
-            gameDisplay.appendChild(boardRow);
-                for (let j = 0; j<3; j++){
+        for (let i = 0 ; i < 9; i++){
                     //creates each gameboard sqr
                     gameboard.push('');
                     const tile = document.createElement('div');
                         tile.classList.add('tile');
                         tile.setAttribute('id', gameboard.length-1)
-                    boardRow.appendChild(tile);
-                }        
+                    gameDisplay.appendChild(tile);               
         }
     //remove play button from screen
-    welcomeDisplay.removeChild(playBtn);
+    view.removeChild(welcomeDisplay);
     }
-    
+    const keepScore = function (){
+        
 
+    }
+    return {createBoard}
 })();
+
 
 
