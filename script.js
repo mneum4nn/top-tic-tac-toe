fceba8fdd2a3e109f32f12f913244977f292ac40
mneum4nn <n3um4nn@proton.me>
2023-11-29 11:22:38 -0600

    turn gameboard function into IIFE

--- script.js
+++ script.js
@@ -13,26 +13,29 @@
 //FACTORY FUNCTION AREA
 //TODO: create gameboard object with board as a local array
     //include the gameboards own methods within this function, creating them as IIFE when possible
-function makeGameboard(){
-    let gameboard = [];
-    for (let i = 0 ; i < 3; i++){
-        //create each row
-        const boardRow = document.createElement('div');
-            boardRow.classList.add('grid-row');
-            boardRow.setAttribute('id', 'row'+i);
-        gameDisplay.appendChild(boardRow);
-            for (let j = 0; j<3; j++){
-                //creates each gameboard sqr
-                gameboard.push('');
-                const tile = document.createElement('div');
-                    tile.classList.add('tile');
-                    tile.setAttribute('id', gameboard.length-1)
-                boardRow.appendChild(tile);
-            }
-    }
+ const game = ( function () {
+    const createBoard = function (){
+        let gameboard = [];
+        for (let i = 0 ; i < 3; i++){
+            //create each row
+            const boardRow = document.createElement('div');
+                boardRow.classList.add('grid-row');
+                boardRow.setAttribute('id', 'row'+i);
+            gameDisplay.appendChild(boardRow);
+                for (let j = 0; j<3; j++){
+                    //creates each gameboard sqr
+                    gameboard.push('');
+                    const tile = document.createElement('div');
+                        tile.classList.add('tile');
+                        tile.setAttribute('id', gameboard.length-1)
+                    boardRow.appendChild(tile);
+                }        
+        }
     //remove play button from screen
     welcomeDisplay.removeChild(playBtn);
+    }
+    
 
-}
+})();
 
 
