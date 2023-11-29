ab7cff850b2642eeb6e5f4aa48b7b66572b3e3ce
mneum4nn <n3um4nn@proton.me>
2023-11-29 11:01:27 -0600

    Create event listener for play button which creates gameboard grid and array

---
+++ script.js
@@ -0,0 +1,38 @@
+//GLOBAL VARIABLE DECLARATIONS
+    //DOM variables
+    const gameDisplay = document.querySelector('.game-page'); 
+    const playBtn = document.getElementById('play-btn');
+    const welcomeDisplay = document.querySelector('.welcome-page');
+
+    //EVENT LISTENERS 
+        playBtn.addEventListener('click', () => {
+            makeGameboard();
+    });    
+
+
+//FACTORY FUNCTION AREA
+//TODO: create gameboard object with board as a local array
+    //include the gameboards own methods within this function, creating them as IIFE when possible
+function makeGameboard(){
+    let gameboard = [];
+    for (let i = 0 ; i < 3; i++){
+        //create each row
+        const boardRow = document.createElement('div');
+            boardRow.classList.add('grid-row');
+            boardRow.setAttribute('id', 'row'+i);
+        gameDisplay.appendChild(boardRow);
+            for (let j = 0; j<3; j++){
+                //creates each gameboard sqr
+                gameboard.push('');
+                const tile = document.createElement('div');
+                    tile.classList.add('tile');
+                    tile.setAttribute('id', gameboard.length-1)
+                boardRow.appendChild(tile);
+            }
+    }
+    //remove play button from screen
+    welcomeDisplay.removeChild(playBtn);
+
+}
+
+
