95f47c42707e6dde61a256ce56bc9dd15d8a6589
mneum4nn <n3um4nn@proton.me>
2023-12-05 11:54:43 -0600

    Make game round functional by separating gameplay functions from gamebord functions

--- script.js
+++ script.js
@@ -4,35 +4,23 @@
     const playBtn = document.getElementById('play-btn');
     const welcomeDisplay = document.querySelector('.welcome-page');
     const view = document.getElementById('view');
-    //EVENT LISTENERS 
-        playBtn.addEventListener('click', () => {
-            game.createBoard();
-    });    
 
+    playBtn.addEventListener('click', (event)=>{
+        const board = game.createBoard();
+        console.log(board);
+
+        return board;
+    });
+
+    let gameboard = [];
 
 //FACTORY FUNCTION AREA
 //TODO: create gameboard object with board as a local array
-    //include the gameboards own methods within this function, creating them as IIFE when possible
-const game = ( function () {
-    let gameboard = [];
+const gameplay = (function (){
     let playerScore = 0; 
     let computerScore = 0;
     let playerTurn = true;
-
-    const createBoard = function (){
-        for (let i = 0 ; i < 9; i++){
-                    //creates each gameboard sqr
-                    gameboard.push('');
-                    const tile = document.createElement('div');
-                        tile.classList.add('tile');
-                        tile.setAttribute('id', gameboard.length-1);
-                        tile.addEventListener('click',(event) => playGame(event.target));
-                    gameDisplay.appendChild(tile);               
-        }
-    //remove play button from screen
-    view.removeChild(welcomeDisplay);
-    }
-    const playGame = function (tile){
+    const playGame = function (tile) {
         let roundOver = false; 
             //create event listener on click for each tile alters gameboard array and gameboard display
             if (playerTurn == true) {
@@ -40,102 +28,83 @@ const game = ( function () {
                 gameboard[xIndex] = 'X';
                 tile.innerHTML = gameboard[xIndex];
                 playerTurn = false;
-                } else 
-                {
-                    let choiceMade = false;
-                    function getRandomNumber() {
-                        return Math.floor(Math.random() * 9);
+                } 
+            else {
+                let oIndex; 
+                function getRandomNumber() {
+                    return Math.floor(Math.random() * 9);
                 }
-                while (choiceMade == false){
-                    let oIndex = getRandomNumber();
-                    if (gameboard[oIndex] == ''){
-                        const chosenTile = document.getElementById(oIndex);
-                        gameboard[oIndex] = 'O'
-                        chosenTile.innerHTML = gameboard[oIndex];
-                        choiceMade==true;
-                    }
+                while (oIndex == null){
+                let rN = getRandomNumber();
+                if (gameboard[rN] == ''){
+                    oIndex = rN;
+                    const chosenTile = document.getElementById(oIndex);
+                    gameboard[oIndex] = 'O'
+                    chosenTile.innerHTML = gameboard[oIndex];
+                    }                            
                 }
                 playerTurn = true;
             }
-            const checkIfPlayerWon =  function (){
-                if (gameboard[0]&&gameboard[1]&&gameboard[2] == 'X'){
-                    playerScore++;
-                    roundOver = true; 
-                } else if (gameboard[3]&&gameboard[4]&&gameboard[5] == 'X'){
-                    playerScore++;
-                    roundOver = true; 
-                }
-                else if (gameboard[6]&&gameboard[7]&&gameboard[8] == 'X'){
-                    playerScore++;
-                    roundOver = true; 
-                }
-                else if (gameboard[0]&&gameboard[3]&&gameboard[6] == 'X'){
-                    playerScore++;
-                    roundOver = true; 
-                }
-                else if (gameboard[1]&&gameboard[4]&&gameboard[7] == 'X'){
-                    playerScore++;
-                    roundOver = true; 
-                }
-                else if (gameboard[2]&&gameboard[5]&&gameboard[8] == 'X'){
-                    playerScore++;
-                    roundOver = true; 
-                }
-                else if (gameboard[0]&&gameboard[4]&&gameboard[8] == 'X'){
-                    playerScore++;
-                    roundOver = true; 
-                }
-                else if (gameboard[2]&&gameboard[4]&&gameboard[6] == 'X'){
-                    playerScore++;
-                    roundOver = true; 
-                }
+
+        function checkForWin(symbol) {
+        // Define all possible winning combinations
+            const winConditions = [
+                [0, 1, 2], [3, 4, 5], [6, 7, 8], // Rows
+                [0, 3, 6], [1, 4, 7], [2, 5, 8], // Columns
+                [0, 4, 8], [2, 4, 6]             // Diagonals
+            ];          
+        // Check each winning condition
+            for (const condition of winConditions) {
+                const [a, b, c] = condition;
+                if (gameboard[a] === symbol && gameboard[b] === symbol && gameboard[c] === symbol) {
+                    roundOver = true;; // Found a winning combination
+                    if (symbol=='X'){
+                            playerScore++;
+                    }
+                    else if (symbol=='O'){
+                            computerScore++;
+                    }
+                } 
             }
-            const checkIfComputerWon = function(){
-                if (gameboard[0]&&gameboard[1]&&gameboard[2] == 'O'){
-                    computerScore++;
-                    roundOver = true; 
-                } else if (gameboard[3]&&gameboard[4]&&gameboard[5] == 'O'){
-                    computerScore++;
-                    roundOver = true; 
-                }
-                else if (gameboard[6]&&gameboard[7]&&gameboard[8] == 'O'){
-                    computerScore++;
-                    roundOver = true; 
-                }               
-                else if (gameboard[0]&&gameboard[3]&&gameboard[6] == 'O'){
-                    playerScore++;
-                    roundOver = true; 
-                }
-                else if (gameboard[1]&&gameboard[4]&&gameboard[7] == 'O'){
-                    playerScore++;
-                    roundOver = true; 
-                }
-                else if (gameboard[2]&&gameboard[5]&&gameboard[8] == 'O'){
-                    playerScore++;
-                    roundOver = true; 
-                }
-                else if (gameboard[0]&&gameboard[4]&&gameboard[8] == 'O'){
-                    playerScore++;
-                    roundOver = true; 
-                }
-                else if (gameboard[2]&&gameboard[4]&&gameboard[6] == 'O'){
-                    playerScore++;
-                    roundOver = true; 
-                }
-            }
+        }
+        checkForWin('X');
+        checkForWin('O');
+        if (roundOver == true) {        
         //once round is over, a button should appear and if clicked we clear the gameboard for a new round
-        const newRoundBtn = document.createElement('button');
-            newRoundBtn.classList.add('game-control');
-            newRoundBtn.setAttribute(id, 'new-round');
-        newRoundBtn.addEventListener('click',()=>{
-            gameboard.splice('');
-            console.log(gameboard);
-            document.querySelector('.tile').innerHTML = ' ';
-            roundOver = false;
-        });
-    }
-    return {createBoard,playGame}
+            const newRoundBtn = document.createElement('button');
+                newRoundBtn.classList.add('game-control');
+                newRoundBtn.setAttribute('id', 'new-round');
+                gameDisplay.appendChild(newRoundBtn);
+                newRoundBtn.addEventListener('click',()=>{
+                    gameboard.splice('');
+                    console.log(gameboard);
+                    document.querySelector('.tile').forEach(tile  => {
+                        tile.innerHTML = ''
+                    });
+                });
+
+            }    
+        }
+        return {playGame};
 })();
+
+const game = ( function () {
+    const createBoard = function (){
+        for (let i = 0 ; i < 9; i++){
+            //creates each gameboard sqr
+            gameboard.push('');
+            const tile = document.createElement('div');
+                tile.classList.add('tile');
+                tile.setAttribute('id', gameboard.length-1);
+                tile.addEventListener('click', () => gameplay.playGame(tile));
+
+            gameDisplay.appendChild(tile);               
+        }
+    //remove play button from screen
+    view.removeChild(welcomeDisplay);
+    };
+    return { createBoard };
+}) ();
 
 
 
